0a716c8bec7975e07ecdf89e0104cc160da5a8ee
ialuy <ialuy@users.noreply.github.com>
2025-12-10 11:27:02 +0100

    Update KeyValueMigrationProcessor test.

--- src/test/kotlin/org/entur/ror/ubelluris/processor/KeyValueMigrationProcessorTest.kt
+++ src/test/kotlin/org/entur/ror/ubelluris/processor/KeyValueMigrationProcessorTest.kt
@@ -4,14 +4,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
-import javax.xml.parsers.DocumentBuilderFactory
 
 class KeyValueMigrationProcessorTest {
 
     private val processor = KeyValueMigrationProcessor()
 
     @Test
-    fun `should zero-pad owner value when it is numeric`(@TempDir tempDir: Path) {
+    fun shouldZeroPadOwnerValueWhenNumeric(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -38,7 +37,7 @@ class KeyValueMigrationProcessorTest {
     }
 
     @Test
-    fun `should zero-pad data-from value when it is numeric`(@TempDir tempDir: Path) {
+    fun shouldZeroPadDataFromValueWhenNumeric(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -65,7 +64,7 @@ class KeyValueMigrationProcessorTest {
     }
 
     @Test
-    fun `should not zero-pad non-numeric values`(@TempDir tempDir: Path) {
+    fun shouldNotZeroPadNonNumericValues(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -92,7 +91,7 @@ class KeyValueMigrationProcessorTest {
     }
 
     @Test
-    fun `should not zero-pad values for keys not in the zero-pad list`(@TempDir tempDir: Path) {
+    fun shouldNotZeroPadValuesForKeysNotPresentInZeroPadList(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -120,7 +119,7 @@ class KeyValueMigrationProcessorTest {
     }
 
     @Test
-    fun `should remove blacklisted KeyValue elements`(@TempDir tempDir: Path) {
+    fun shouldRemoveBlacklistedKeyValueElements(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -153,7 +152,7 @@ class KeyValueMigrationProcessorTest {
     }
 
     @Test
-    fun `should remove all blacklisted keys`(@TempDir tempDir: Path) {
+    fun shouldRemoveAllBlacklistedKeys(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -196,7 +195,7 @@ class KeyValueMigrationProcessorTest {
     }
 
     @Test
-    fun `should handle multiple KeyValue elements correctly`(@TempDir tempDir: Path) {
+    fun shouldHandleMultipleKeyValueElementsCorrectly(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -232,62 +231,17 @@ class KeyValueMigrationProcessorTest {
 
         val result = xmlFile.readText()
 
-        // Zero-padded values
         assertThat(result).contains("<Value>005</Value>")
         assertThat(result).contains("<Value>099</Value>")
 
-        // Removed blacklisted key
         assertThat(result).doesNotContain("trafikverket-name")
 
-        // Preserved non-targeted key
         assertThat(result).contains("<Key>keep-me</Key>")
         assertThat(result).contains("<Value>123</Value>")
     }
 
     @Test
-    fun `should preserve XML structure and other elements`(@TempDir tempDir: Path) {
-        val inputXml = """
-            <?xml version="1.0" encoding="UTF-8"?>
-            <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
-                <dataObjects>
-                    <StopPlace id="SAM:StopPlace:1" version="1">
-                        <Name>Test Stop</Name>
-                        <keyList>
-                            <KeyValue>
-                                <Key>owner</Key>
-                                <Value>8</Value>
-                            </KeyValue>
-                        </keyList>
-                        <Centroid>
-                            <Location>
-                                <Longitude>12.345</Longitude>
-                                <Latitude>67.890</Latitude>
-                            </Location>
-                        </Centroid>
-                    </StopPlace>
-                </dataObjects>
-            </PublicationDelivery>
-        """.trimIndent()
-
-        val xmlFile = tempDir.resolve("test.xml").toFile()
-        xmlFile.writeText(inputXml)
-
-        processor.process(xmlFile)
-
-        val result = xmlFile.readText()
-
-        // Check that KeyValue was processed
-        assertThat(result).contains("<Value>008</Value>")
-
-        // Check that other elements are preserved
-        assertThat(result).contains("<Name>Test Stop</Name>")
-        assertThat(result).contains("<Longitude>12.345</Longitude>")
-        assertThat(result).contains("<Latitude>67.890</Latitude>")
-        assertThat(result).contains("id=\"SAM:StopPlace:1\"")
-    }
-
-    @Test
-    fun `should handle empty keyList`(@TempDir tempDir: Path) {
+    fun shouldHandleEmptyKeyList(@TempDir tempDir: Path) {
         val inputXml = """
             <?xml version="1.0" encoding="UTF-8"?>
             <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
@@ -309,35 +263,4 @@ class KeyValueMigrationProcessorTest {
         assertThat(result).contains("<keyList>")
     }
 
-    @Test
-    fun `should produce valid XML output`(@TempDir tempDir: Path) {
-        val inputXml = """
-            <?xml version="1.0" encoding="UTF-8"?>
-            <PublicationDelivery xmlns="http://www.netex.org.uk/netex">
-                <dataObjects>
-                    <StopPlace id="SAM:StopPlace:1">
-                        <keyList>
-                            <KeyValue>
-                                <Key>owner</Key>
-                                <Value>9</Value>
-                            </KeyValue>
-                        </keyList>
-                    </StopPlace>
-                </dataObjects>
-            </PublicationDelivery>
-        """.trimIndent()
-
-        val xmlFile = tempDir.resolve("test.xml").toFile()
-        xmlFile.writeText(inputXml)
-
-        processor.process(xmlFile)
-
-        // Verify the output is valid XML by parsing it
-        val factory = DocumentBuilderFactory.newInstance()
-        val builder = factory.newDocumentBuilder()
-        val doc = builder.parse(xmlFile)
-
-        assertThat(doc).isNotNull
-        assertThat(doc.documentElement.tagName).isEqualTo("PublicationDelivery")
-    }
 }
