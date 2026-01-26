36e44000e5191e666706678db9536deee9011b4a
ialuy <ialuy@users.noreply.github.com>
2026-01-26 11:07:35 +0100

    fix-transport-mode-normalization: Remove unused field publicCode in StopPlacePurgingParsingContext.

--- src/main/kotlin/org/entur/ror/ubelluris/sax/plugins/StopPlacePurgingParsingContext.kt
+++ src/main/kotlin/org/entur/ror/ubelluris/sax/plugins/StopPlacePurgingParsingContext.kt
@@ -1,7 +1,6 @@
 package org.entur.ror.ubelluris.sax.plugins
 
 data class StopPlacePurgingParsingContext(
-    var publicCode: String? = null,
     var currentQuayId: String? = null,
     var quayHasPublicCode: Boolean = false
 )
